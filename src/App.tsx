fb82a34c8c71d6afd23009ac5ff60553ac8487c1
sarvesh <152284157+innovest2025@users.noreply.github.com>
2025-07-31 17:02:45 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage.tsx";
 import InnovestHackPage from "./pages/InnovestHackPage.tsx";
-//import EventsGallery from "./components/gallery_header.tsx";
+import EventsGallery from "./components/gallery_header.tsx";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/innovesthack" element={<InnovestHackPage />} />
-     //<Route path="/gallery_header" element={<EventsGallery />} />
+      <Route path="/gallery_header" element={<EventsGallery />} />
     </Routes>
   );
 }
