3e86994d4ce534f816a6bfebdaecd42ea944073a
sarvesh <152284157+innovest2025@users.noreply.github.com>
2025-08-01 17:05:06 +0530

    Update Events.tsx

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -191,7 +191,7 @@ const Schedule = () => {
 
   const handleKnowMore = (day: string) => {
     if (day === "Day 2") {
-      window.location.href = "/InnovestHack";
+      window.location.href = "/#InnovestHack";
     } else if (day === "Day 1" || day === "Day 3") {
       setSelectedDay(day);
       setModalOpen(true);
