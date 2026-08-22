90a27c00ac39dc250623be78391a67273fdbb113
sarvesh <152284157+innovest2025@users.noreply.github.com>
2026-08-22 22:05:31 +0530

    Enhance Vite config with path alias and server options
    Updated Vite configuration to include path aliasing and improved server settings.

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
-    host: '0.0.0.0',
+    port: 3000,
+    strictPort: true,
+    host: true,
+    open: true,
+
     allowedHosts: [
-      'innovest-of83.onrender.com'
-    ]
+      "innovest-of83.onrender.com",
+    ],
+
+    watch: {
+      usePolling: true,
+      interval: 100,
+    },
   },
 
   preview: {
-    host: '0.0.0.0',
+    host: true,
     allowedHosts: [
-      'innovest-of83.onrender.com'
-    ]
-  }
-})
+      "innovest-of83.onrender.com",
+    ],
+  },
+});
